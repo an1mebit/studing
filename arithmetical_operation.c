737d676ffe285000e486d9aec970775208a708e1
an1mebit <baldej@mail.ru>
2020-10-25 15:11:09 +0300

    adding division

--- arithmetical_operation.c
+++ arithmetical_operation.c
@@ -2,14 +2,16 @@
 
 int main (void)
 {
-    int a,b,c,d,e;
+    int a,b,c,d,e,f;
     printf("a = "); scanf("%d", &a);
     printf("b = "); scanf("%d", &b);
     c=a+b;
     printf("Sum = %d", c);
     d=a-b;
-    printf("Ded = %d", d);
+    printf("Sub = %d", d);
     e=a*b;
     printf("Mul = %d", e);
+    f=a/b;
+    printf("Div = %d", f);
     return 0;
 }
