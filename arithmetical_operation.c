d84a69a57b3637712a56df30fbeccdf2fc1dee3e
an1mebit <baldej@mail.ru>
2020-10-25 15:08:03 +0300

    replacing division with subtraction, adding multiplication

--- arithmetical_operation.c
+++ arithmetical_operation.c
@@ -2,12 +2,14 @@
 
 int main (void)
 {
-    int a,b,c,d;
+    int a,b,c,d,e;
     printf("a = "); scanf("%d", &a);
     printf("b = "); scanf("%d", &b);
     c=a+b;
     printf("Sum = %d", c);
     d=a-b;
-    printf("Div = %d", d);
+    printf("Ded = %d", d);
+    e=a*b;
+    printf("Mul = %d", e);
     return 0;
 }
