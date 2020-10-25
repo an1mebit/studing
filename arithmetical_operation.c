0e4407a9dca621ff1a8e9e0619f52988da1a3e7a
an1mebit <baldej@mail.ru>
2020-10-25 15:42:24 +0300

    test

--- arithmetical_operation.c
+++ arithmetical_operation.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
+void test (int sum, int sub, int mul, float div);
+
 int main (void)
 {
-    int a,b,c,d,e,f;
+    int a,b,c,d,e = 0;
+    float f = 0;
     printf("a = "); scanf("%d", &a);
     printf("b = "); scanf("%d", &b);
     c=a+b;
-    printf("Sum = %d", c);
+    printf("Sum = %d\n", c);
     d=a-b;
-    printf("Sub = %d", d);
+    printf("Sub = %d\n", d);
     e=a*b;
-    printf("Mul = %d", e);
-    f=a/b;
-    printf("Div = %d", f);
+    printf("Mul = %d\n", e);
+    f= (float) a / b;
+    printf("Div = %.3f\n", f);
+    test (c, d, e, f);
     return 0;
 }
+void test (int sum, int sub, int mul, float div) {
+    printf("sum is %d\n", sum);
+    printf("substraction is %d\n", sub);
+    printf("multiplication is %d\n", mul);
+    printf("division is %.3f\n", div);
+}  
